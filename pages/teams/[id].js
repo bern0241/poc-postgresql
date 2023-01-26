53b0fe032585981a23945fd0df8fa14dce515dfd
[Justin Bernard] <[bern0241@algonquinlive.com]>
2023-01-25 20:55:51 -0500

    Setup teams api

---
+++ pages/teams/[id].js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TeamDetails = () => {
+
+  return (
+    <div>
+        <h1>TEAM DETAIL!</h1>
+    </div>
+  )
+}
+
+export default TeamDetails;
--- pages/teams/index.js
+++ pages/teams/index.js
@@ -1,7 +1,79 @@
-import React from "react";
+import React, {useEffect, useState} from 'react'
 
-function index() {
-  return <div>teams index</div>;
+const Teams = () => {
+  const [team, setTeam] = useState();
+
+  useEffect(() => {
+    async function getTeam() {
+      const resp = await fetch('/api/teams');
+      let data = await resp.json();
+      console.log(data);
+      setTeam(data);
+    }
+    getTeam();
+  }, [])
+
+  async function handlePost() {
+    console.log("TEST");
+    try {
+        const dummyObject = {
+            first_name: 'Justin',
+            last_name: 'Bernard',
+            email: 'justi31n@email.com'
+          }
+          
+        await fetch('api/teams', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({team:dummyObject}),
+        }).catch(error => {
+          console.log(error)
+        })
+
+    } catch (error) {
+      console.error(error.message);
+    }
+
+  }
+
+  return (
+    <div>
+        <h1>TEAMS PAGE!</h1>
+        {team && team.map(team => (
+          <div className='bg-red-700' key={team.id}>
+            <p>{team.first_name}</p>
+            <p>{team.last_name}</p>
+          </div>
+        ))}
+        <button onClick={handlePost} class='bg-green-500'>Test Me</button>
+    </div>
+  )
 }
 
-export default index;
+
+// export async function getServerSideProps() {
+//   const options = {
+//       method: 'GET',
+//       headers: {
+//           'Content-Type': 'application/json'
+//       }
+//   };
+
+//   const resp = await fetch('api/hello', options);
+//   let data = await resp.json();
+
+//   if (!data) {
+//       return {
+//           notFound: true,
+//       }
+//   }
+//   return {
+//       props: {
+//           team: data
+//       }
+//   }
+// }
+
+export default Teams;
