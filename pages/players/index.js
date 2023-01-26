3e09c78752713a5953c21776382688dfdfb0dd10
Verity Stevens <43241358+stev0298@users.noreply.github.com>
2023-01-25 19:43:26 -0500

    create file structure

---
+++ pages/players/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function index() {
+  return <div>players index</div>;
+}
+
+export default index;
---
+++ pages/teams/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function index() {
+  return <div>teams index</div>;
+}
+
+export default index;
