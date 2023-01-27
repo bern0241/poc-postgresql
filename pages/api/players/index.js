0b836cf3470c1921d45177d561ec3b72ff90f6fc
tran0460 <tran0460@algonquinlive.com>
2023-01-26 20:23:21 -0500

    setup get route for players

--- pages/api/players/index.js
+++ pages/api/players/index.js
@@ -1,1 +1,25 @@
-s
+import { knex } from "../../../lib/utils";
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    try {
+      await knex
+        .from("players")
+        .select("id", "first_name", "last_name", "email")
+        .then((players) => {
+          res.status(200).json(players);
+        });
+    } catch (error) {
+      res.status(400).json({ error: "Team not found!" });
+    }
+  }
+  if (req.method === "POST") {
+    try {
+      const { player } = req.body;
+      await knex("players").insert(player);
+      res.status(200).json({ message: "Player added" });
+    } catch (error) {
+      res.status(400).json({ error: "Player not found!" });
+    }
+  }
+}
