851c21d865a0fd7d068ee3ebec6baa23f83ed2c8
tran0460 <tran0460@algonquinlive.com>
2023-01-26 20:23:39 -0500

    retrieve data from get route

--- pages/players/index.js
+++ pages/players/index.js
@@ -1,6 +1,19 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 function index() {
+  const getPlayers = async () => {
+    try {
+      await fetch("api/players").then(async (resp) => {
+        console.log(await resp.json());
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getPlayers();
+  }, []);
   return <div>players index</div>;
 }
 
