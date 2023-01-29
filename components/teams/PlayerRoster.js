c024236c6933c2fde2adfa88f34cdeefa56bfc86
[Justin Bernard] <[bern0241@algonquinlive.com]>
2023-01-29 01:12:45 -0500

    Do player modal

--- components/teams/PlayerRoster.js
+++ components/teams/PlayerRoster.js
@@ -1,28 +1,16 @@
 import React, {useState, useEffect} from 'react'
-// import Link from 'next/link'
+import ViewPlayerModal from '@/components/teams/ViewPlayerModal.js'
 
 const PlayerRoster = ({players}) => {
 
-    async function handleRemove(e, id) {
-        e.stopPropagation();
-        try {
-            await fetch('api/teams/' + id, {
-            method: 'DELETE'
-            })
-            .then((result) => {
-                setTeams(teams.filter(team => team.id !== id))
-            })
-            .catch(error => {
-                console.log(error)
-            })
+    const [openPlayerModal, setOpenPlayerModal] = useState(false);
 
-        } catch (error) {
-        console.error(error.message);
-        }
+    async function handlePlayerSelect() {
+        setOpenPlayerModal(true);
     }
-
   
   return (
+<>
 <div class="mt-5 max-w-[80em] mx-auto relative overflow-x-auto shadow-md sm:rounded-lg">
     <table class="text-center text-[1rem] w-full text-sm text-left text-blue-100 dark:text-blue-100">
         <thead class="text-xs text-center text-white uppercase bg-blue-500 border-b border-black-400 dark:text-white">
@@ -50,7 +38,7 @@ const PlayerRoster = ({players}) => {
         </thead>
         <tbody>
             {players && players.map((player) => (
-                <tr onClick={() => handleTeamSelected(team.id)} class="bg-gray-000 border-b border-gray-300 hover:bg-gray-100 text-gray-600">
+                <tr onClick={() => handlePlayerSelect(player)} class="bg-gray-000 border-b border-gray-300 hover:bg-gray-100 text-gray-600">
                     <th scope="row" class="text-left px-6 py-4 font-medium whitespace-nowrap dark:text-blue-100">
                         {player.first_name} {player.last_name}
                     </th>
@@ -77,6 +65,10 @@ const PlayerRoster = ({players}) => {
     </table>
 </div>
 
+{openPlayerModal && (
+    <ViewPlayerModal setOpenPlayerModal={setOpenPlayerModal} />
+)}
+</>
 
   )
 }
