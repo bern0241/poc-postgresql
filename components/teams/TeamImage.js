eaf218567876b9113bbd2992017e5844c0d81762
[Justin Bernard] <[bern0241@algonquinlive.com]>
2023-01-29 21:10:46 -0500

    File image upload for team works

--- components/teams/TeamImage.js
+++ components/teams/TeamImage.js
@@ -7,15 +7,12 @@ import { createClient } from '@supabase/supabase-js';
 
 const anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrc2tua3dneGhhbmh0aWxueWNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQ2MDMwOTIsImV4cCI6MTk5MDE3OTA5Mn0.wv3HM3vosHyiJrhMmha4ocHQxcvwxiUBq9QKHeFHCkc'
 
-const supabase = createClient('https://cksknkwgxhanhtilnycs.supabase.co', anonKey, {
-  headers: {
-    apiKey: anonKey,
-    // Authorization: `Bearer ${jwt}`
-  }});
+const supabase = createClient('https://cksknkwgxhanhtilnycs.supabase.co', anonKey);
+
+const supabaseBucketUrl = 'https://cksknkwgxhanhtilnycs.supabase.co/storage/v1/object/public/team-image/';
 
 
-
-  function TeamImage({imagesrc, editable}) {
+function TeamImage({imagesrc, editable}) {
     const [isLoading, setLoading] = useState(true);
     const searchParams = useSearchParams();
     const [image, setImage] = useState(null);
@@ -37,26 +34,23 @@ const supabase = createClient('https://cksknkwgxhanhtilnycs.supabase.co', anonKe
     
     try {
         // upload image, then fetch url
-        // const {data, error} = await supabase.storage.from('s4sw6j_1/team-image').upload(`${Date.now()} ${image.name}`, image)
-
+        const {data, error} = await supabase.storage.from('team-image').upload(`${Date.now()}_${image.name}`, image)
         //get images
-        const {data,error} = await supabase.storage.from('public/team-image22').list()
+        // const {data,error} = await supabase.storage.from('team-image').list()
 
-        console.log('Data:', data);
-        console.log('DataKey:', data.Key);
+        const newImageUrl = `${supabaseBucketUrl}${data.path}`
+        console.log('New Image:', newImageUrl);
 
         if (error) {
           console.error(error);
         }
-
-        return;
        
         const resp = fetch('http://localhost:3000/api/team-image/' + id, {
           method: 'PATCH',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({newImageUrl: 'https://i.etsystatic.com/22851110/r/il/f6d419/2336545826/il_1588xN.2336545826_al2n.jpg'})
+          body: JSON.stringify({newImageUrl: newImageUrl})
         }).then(() => {
           router.refresh();
         })
@@ -65,21 +59,38 @@ const supabase = createClient('https://cksknkwgxhanhtilnycs.supabase.co', anonKe
     }
   }
 
-  return (
-    <div class='group overflow-hidden rounded-lg'>
-          {/* <input class='' type="file" accept={"image/*"} 
-            onChange={(e) => setImage(e.target.files[0])}/> */}
-            <img 
-            alt="Team Image"
-            src={imagesrc ? imagesrc : "https://bit.ly/placeholder-img"}
-            layout="fill"
-            objectFit="cover"
-            className={editable ? 'border cursor-pointer max-w-[9em] max-h-[10em] group-hover:opacity-75' : 
-            'max-w-[9em] max-h-[10em]'}
-            />
+  if (editable) {
+    return (
+      <div class='group overflow-hidden rounded-lg'>
+          <input class='hidden' id='file' type="file" multiple accept={"image/*"} 
+            onChange={(e) => setImage(e.target.files[0])}/>
+              <label for='file'>
+              <img 
+              alt="Team Image"
+              src={imagesrc ? imagesrc : "https://bit.ly/placeholder-img"}
+              layout="fill"
+              objectFit="cover"
+              className={editable ? 'border border-gray-300 cursor-pointer max-w-[9em] max-h-[10em] group-hover:opacity-50' : 
+              'max-w-[9em] max-h-[10em]'}
+              />
+            </label>
         </div>
-
-  )
+    )
+  } else {
+    return (
+      <div class='group overflow-hidden rounded-lg'>
+              <img 
+              alt="Team Image"
+              src={imagesrc ? imagesrc : "https://bit.ly/placeholder-img"}
+              layout="fill"
+              objectFit="cover"
+              className={editable ? 'border border-gray-300 cursor-pointer max-w-[9em] max-h-[10em] group-hover:opacity-50' : 
+              'max-w-[9em] max-h-[10em]'}
+              />
+        </div>
+    )
+  }
+  
 }
 
 export default TeamImage
