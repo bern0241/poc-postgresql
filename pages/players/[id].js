d9782bfab33cadd7d6c62d05087393f745e8047c
kaur0762 <kaur0762@algonquinlive.com>
2023-01-30 12:50:05 -0500

    edit form working for details page

--- pages/players/[id].js
+++ pages/players/[id].js
@@ -4,6 +4,9 @@ import { useRouter } from "next/router";
 function index() {
   const [player, setPlayer] = useState({});
   const [playerTeams, setPlayerTeams] = useState([]);
+
+  const [visibilityUpdate, setVisibilityUpdate] = useState(false);
+
   const router = useRouter();
   const { id } = router.query;
 
@@ -27,24 +30,29 @@ function index() {
    * email: 'john@mail.com'
    * })
    */
+
+  //UPDATE PLAYER
+
   const updatePlayer = async (
     playerData = {
-      first_name: `Joe`,
-      last_name: `Doe`,
-      email: `${(Math.random() * 100).toFixed(0)}@email.com`,
+      first_name: firstNameRef.current.value,
+      last_name: lastNameRef.current.value,
+      email: emailRef.current.value,
     }
   ) => {
+    setVisibilityUpdate(false);
     if (!id) return;
     try {
       await fetch(`/api/players/${id}`, {
         method: "PATCH",
         body: JSON.stringify(playerData),
       }).then((resp) => {
-        if (resp.ok) getPlayer();
+        if (resp.ok) getPlayers();
       });
     } catch (err) {
       console.warn(err);
     }
+
   };
   const deletePlayer = async () => {
     if (!id) return;
@@ -82,9 +90,58 @@ function index() {
           )}
         </p>
         <button onClick={deletePlayer} class="shadow bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded mt-3 w-24" >Delete</button>
-        <button onClick={() => updatePlayer()} class="shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded m-3 w-24" >Update</button>
+        <button onClick={() => {
+                  setVisibilityUpdate(true);
+                }} class="shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded m-3 w-24" >Update</button>
       </div>
       </div>
+
+      <div id="update-form" style={{display: visibilityUpdate?"block":"none"}} class="  flex flex-col justify-center items-center mt-8 bg-grey-400 flex flex-col h-screen justify-center items-cente">
+          <form action="/send-data-here" method="post" class="w-full max-w-md border-2 p-5 m-auto mt-auto">
+          <div class="md:flex md:items-center mb-6">
+              <div class="md:w-1/3">
+                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                  First Name
+                </label>
+              </div>
+              <div class="md:w-2/3">
+                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" ref = {firstNameRef} type="text" placeholder="Jane"/>
+              </div>
+            </div>
+            <div class="md:flex md:items-center mb-6">
+              <div class="md:w-1/3">
+                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                  Last Name
+                </label>
+              </div>
+              <div class="md:w-2/3">
+                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" ref = {lastNameRef} type="text" placeholder="Doe"/>
+              </div>
+            </div>
+            <div class="md:flex md:items-center mb-6">
+              <div class="md:w-1/3">
+                <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                  Email Address
+                </label>
+              </div>
+              <div class="md:w-2/3">
+                <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" ref = {emailRef} type="email" placeholder="jane.doe@gmail.com"/>
+              </div>
+            </div>
+            <div class="md:flex md:items-center">
+              <div class="md:w-1/3"></div>
+              <div class="md:w-2/3">
+                <button class="shadow bg-black hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button"
+                onClick={() => {
+                  updatePlayer();
+                }}>
+                  Update
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+
 
     </>
   );
