4eaecef20b92bf9729736d9f85450754a05ca246
Verity Stevens <43241358+stev0298@users.noreply.github.com>
2023-01-31 01:42:45 -0500

    update player removal modal

--- components/teams/DeleteTeamModal.js
+++ components/teams/DeleteTeamModal.js
@@ -81,7 +81,7 @@ const DeleteTeamModal = ({ setDeleteModal, team }) => {
                 />
               )}
               {/* <svg aria-hidden="true" className="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg> */}
-              <h3 className="mb-2 text-lg font-normal text-white/75 dark:text-gray-400">
+              <h3 className="mb-4 text-lg font-normal text-white/75 dark:text-gray-400">
                 Are you sure you want to delete this team?
                 <br />
               </h3>
